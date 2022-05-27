954ba686b98e1e6c1525646ba738507fac760220
Cardes <mail@vaduz.de>
2022-05-27 15:58:28 +0200

    copy .xml files around because 7days doesn't like readonly files

--- gameimages/7daystodie/src/entrypoint.sh
+++ gameimages/7daystodie/src/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-exec /mygame/gamefiles/./startserver.sh -configfile=/mygame/gamefiles/serverconfig.xml
+#workaround for access permission denied 
+cp -f /mygame/gamefiles/serverconfig.xml /mygame/gamefiles/serverconfig_working.xml
+cp -f /mygame/saves/serveradmin.xml /mygame/saves/serveradmin_working.xml 
+exec /mygame/gamefiles/./startserver.sh -configfile=/mygame/gamefiles/serverconfig_working.xml
